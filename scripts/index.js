176525251e5bf0fb9b8f5fdc1381891d7b37e473
Whidev <Whijlp@outlook.com>
2024-09-04 21:56:29 -0500

    nueva actualizacion de mi proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-let showPopup = document.getElementsByClassName("profile__edit-button");
+const openProfilePopup = document.querySelector(".profile__edit-button");
+const formsPopup = document.querySelector(".forms");
+const closeProfilePopup = document.querySelector(".forms__close-button");
+
+openProfilePopup.addEventListener("click", function () {
+  formsPopup.classList.remove("forms");
+  console.log("se ha echo clic en el boton");
+});
+
+closeProfilePopup.addEventListener("click", function () {
+  formsPopup.classList.add("forms");
+  console.log("se ha cerrado la ventana");
+});
