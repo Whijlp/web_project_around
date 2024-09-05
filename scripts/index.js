62c08a6fb6a550d61d2624b34b751c445a257dd1
Whidev <Whijlp@outlook.com>
2024-09-05 11:38:16 -0500

    nueva actulizacion de mi proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,16 @@
 const openProfilePopup = document.querySelector(".profile__edit-button");
 const formsPopup = document.querySelector(".forms");
 const closeProfilePopup = document.querySelector(".forms__close-button");
+const overlayContainer = document.querySelector(".body__overlay");
 
 openProfilePopup.addEventListener("click", function () {
-  formsPopup.classList.remove("forms");
+  formsPopup.classList.add("forms_show");
+  overlayContainer.classList.add("overlay_show");
   console.log("se ha echo clic en el boton");
 });
 
 closeProfilePopup.addEventListener("click", function () {
-  formsPopup.classList.add("forms");
+  formsPopup.classList.remove("forms_show");
+  overlayContainer.classList.remove("overlay_show");
   console.log("se ha cerrado la ventana");
 });
