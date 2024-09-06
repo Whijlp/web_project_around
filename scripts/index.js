67e946da0e14e25af77186f38fa1228acee8ee05
Whidev <Whijlp@outlook.com>
2024-09-06 09:59:31 -0500

    nueva actulizacion de mi proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const openProfilePopup = document.querySelector(".profile__edit-button");
-const formsPopup = document.querySelector(".pupop");
+const formsPopup = document.querySelector(".popup");
 const closeProfilePopup = document.querySelector(".forms__close-button");
-const overlayContainer = document.querySelector(".body__overlay");
+const overlayContainer = document.querySelector(".popup__overlay");
 const formElement = document.querySelector(".forms__submit-button");
 const nameInput = document.getElementById("nombre");
 const jobInput = document.getElementById("job_info");
@@ -9,13 +9,13 @@ const jobInput = document.getElementById("job_info");
 console.log(nameInput);
 
 openProfilePopup.addEventListener("click", function () {
-  formsPopup.classList.add("pupop__show");
+  formsPopup.classList.add("popup__show");
   overlayContainer.classList.add("overlay_show");
   console.log("se ha echo clic en el boton");
 });
 
 closeProfilePopup.addEventListener("click", function () {
-  formsPopup.classList.remove("pupop__show");
+  formsPopup.classList.remove("popup__show");
   overlayContainer.classList.remove("overlay_show");
   console.log("se ha cerrado la ventana");
 });
@@ -27,5 +27,5 @@ function handleProfileFormSubmit(evt) {
   const jobElement = document.querySelector(".profile__subtitle");
   nameElement.textContent = nameInput.value;
   jobElement.textContent = jobInput.value;
-  formsPopup.classList.remove("pupop__show");
+  formsPopup.classList.remove("popup__show");
 }
