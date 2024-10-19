ef8482f49bcc08533965e834a0723c9756340e24
Whidev <Whijlp@outlook.com>
2024-10-19 17:06:42 -0500

    actualizacion js

--- scripts/index.js
+++ scripts/index.js
@@ -114,7 +114,7 @@ function fillFormInputs() {
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
   titleNewCard.value = namePhotoElement.textContent;
-  photoNewCard.value = "Enlace de la imagen";
+  photoNewCard.value = "https://www.ejemplo.com";
 }
 fillFormInputs();
 
@@ -156,47 +156,3 @@ formsPopupNewPlaces.addEventListener("click", (evt) => {
     formsPopupNewPlaces.close();
   }
 });
-
-function validProfile() {
-  const jobInfoError = document.querySelector("#job_info-error");
-  jobInfoError.classList.remove("form__input-error-invalid");
-  const nombreError = document.querySelector("#nombre-error");
-  nombreError.classList.remove("form__input-error-invalid");
-
-  if (nameInput.checkValidity() && jobInput.checkValidity()) {
-    formElement.disabled = false;
-  } else {
-    formElement.disabled = true;
-    if (!jobInput.checkValidity()) {
-      jobInfoError.classList.add("form__input-error-invalid");
-    }
-    if (!nameInput.checkValidity()) {
-      nombreError.classList.add("form__input-error-invalid");
-    }
-  }
-}
-nameInput.addEventListener("input", validProfile);
-jobInput.addEventListener("input", validProfile);
-validProfile();
-
-function validCard() {
-  const titleCardError = document.querySelector("#form__title-error");
-  titleCardError.classList.remove("form__input-error-card-invalid");
-  const urlCardError = document.querySelector("#form__url-error");
-  urlCardError.classList.remove("form__input-error-card-invalid");
-
-  if (titleNewCard.checkValidity() && photoNewCard.checkValidity()) {
-    createCardButton.disabled = false;
-  } else {
-    createCardButton.disabled = true;
-    if (!photoNewCard.checkValidity()) {
-      urlCardError.classList.add("form__input-error-card-invalid");
-    }
-    if (!titleNewCard.checkValidity()) {
-      titleCardError.classList.add("form__input-error-card-invalid");
-    }
-  }
-}
-titleNewCard.addEventListener("input", validCard);
-photoNewCard.addEventListener("input", validCard);
-validCard();
---
+++ scripts/validate.js
@@ -0,0 +1,57 @@
+const settingsValidation = {
+  formSelector: ".form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(settings.formSelector);
+};
+
+/*
+function validProfile() {
+  const jobInfoError = document.querySelector("#job_info-error");
+  jobInfoError.classList.remove("form__input-error-invalid");
+  const nombreError = document.querySelector("#nombre-error");
+  nombreError.classList.remove("form__input-error-invalid");
+
+  if (nameInput.checkValidity() && jobInput.checkValidity()) {
+    formElement.disabled = false;
+  } else {
+    formElement.disabled = true;
+    if (!jobInput.checkValidity()) {
+      jobInfoError.classList.add("form__input-error-invalid");
+    }
+    if (!nameInput.checkValidity()) {
+      nombreError.classList.add("form__input-error-invalid");
+    }
+  }
+}
+nameInput.addEventListener("input", validProfile);
+jobInput.addEventListener("input", validProfile);
+validProfile();
+
+function validCard() {
+  const titleCardError = document.querySelector("#form__title-error");
+  titleCardError.classList.remove("form__input-error-card-invalid");
+  const urlCardError = document.querySelector("#form__url-error");
+  urlCardError.classList.remove("form__input-error-card-invalid");
+
+  if (titleNewCard.checkValidity() && photoNewCard.checkValidity()) {
+    createCardButton.disabled = false;
+  } else {
+    createCardButton.disabled = true;
+    if (!titleNewCard.checkValidity()) {
+      titleCardError.classList.add("form__input-error-card-invalid");
+    }
+    if (!photoNewCard.checkValidity()) {
+      urlCardError.classList.add("form__input-error-card-invalid");
+    }
+  }
+}
+titleNewCard.addEventListener("input", validCard);
+photoNewCard.addEventListener("input", validCard);
+validCard();*/
