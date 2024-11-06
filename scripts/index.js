6887ca22778c4a0059ab7e1251f00690ab887d12
Whidev <Whijlp@outlook.com>
2024-11-06 14:30:59 -0500

    actulizacion modulos utils.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,23 @@
 import { settingsValidation, FormValidator } from "./FormValidate.js";
 import Card from "./Card.js";
-import { initialCards, openImage } from "./utils.js";
+import {
+  initialCards,
+  openImage,
+  handleOpenPopup,
+  handleClosePopup,
+} from "./utils.js";
 
-const openProfilePopup = document.querySelector(".profile__edit-button");
-const openNewPlacePopup = document.querySelector(".profile__add-button");
-const formsPopup = document.querySelector(".popup");
 const formsPopupNewPlaces = document.querySelector("#popup__new-places");
-const closeProfilePopup = document.querySelector(".forms__close-button");
-const closeFormProfil = document.querySelector(".forms-profile-button");
-const closePlacePopup = document.querySelector("#place-close-button");
-const overlayContainer = document.querySelector(".popup__overlay");
 const formElement = document.querySelector("#perfil-button");
 const jobInput = document.getElementById("job_info");
 const nameElement = document.querySelector(".profile__title");
-const namePhotoElement = document.querySelector(".forms__title");
 const jobElement = document.querySelector(".profile__subtitle");
-const dialogPopup = document.querySelector(".popup_dialog");
-const cardTemplate = document.querySelector("#card-template");
 const cardContainer = document.querySelector(".elements");
-const closeImgPopup = document.querySelector(".popup__close-button");
 const createCardForm = document.querySelector("#create-card");
 const titleNewCard = document.querySelector("#titulo");
 const photoNewCard = document.querySelector("#photo_info");
 const formEditProfile = document.getElementById("form_edit-profile");
 const nameInput = formEditProfile.querySelector("#nombre");
-const inputForm = document.querySelectorAll(".form__input");
 
 initialCards.forEach((item) => {
   const card = new Card(item, openImage);
@@ -46,13 +39,13 @@ createCardForm.addEventListener("submit", (evt) => {
   formsPopupNewPlaces.close();
 });
 
-const validateFormProfile = new FormValidator(
+export const validateFormProfile = new FormValidator(
   settingsValidation,
   formEditProfile
 );
 validateFormProfile.enableValidation();
 
-const validateFormNewBike = new FormValidator(
+export const validateFormNewBike = new FormValidator(
   settingsValidation,
   createCardForm
 );
@@ -65,77 +58,4 @@ function handleProfileFormSubmit(evt) {
   handleClosePopup(evt);
 }
 
-function handleClosePopup(evt) {
-  evt.preventDefault();
-  formsPopup.classList.remove("popup__show");
-  overlayContainer.classList.remove("overlay_show");
-  resetFillImput(evt);
-  resetFillCard(evt);
-  formsPopupNewPlaces.close();
-  dialogPopup.close();
-  validateFormProfile._resetForm();
-  validateFormNewBike._resetForm();
-}
-function resetFillImput() {
-  formEditProfile.reset();
-  nameInput.value = nameElement.textContent;
-  jobInput.value = jobElement.textContent;
-}
-
-function resetFillCard() {
-  createCardForm.reset();
-  titleNewCard.value = namePhotoElement.textContent;
-  photoNewCard.value = "https://www.ejemplo.com";
-}
-function handleOpenPopup() {
-  formsPopup.classList.add("popup__show");
-  overlayContainer.classList.add("overlay_show");
-}
-function fillFormInputs() {
-  nameInput.value = nameElement.textContent;
-  jobInput.value = jobElement.textContent;
-  titleNewCard.value = namePhotoElement.textContent;
-  photoNewCard.value = "https://www.ejemplo.com";
-}
-fillFormInputs();
-
-openProfilePopup.addEventListener("click", handleOpenPopup);
-
-closeProfilePopup.addEventListener("click", handleClosePopup);
-
-closeFormProfil.addEventListener("click", handleClosePopup);
-
 formElement.addEventListener("click", handleProfileFormSubmit);
-
-overlayContainer.addEventListener("click", handleClosePopup);
-
-document.addEventListener("keydown", (evt) => {
-  if (evt.key === "Escape") {
-    handleClosePopup(evt);
-  }
-});
-
-openNewPlacePopup.addEventListener("click", () => {
-  formsPopupNewPlaces.showModal();
-});
-
-closePlacePopup.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  formsPopupNewPlaces.close();
-  handleClosePopup(evt);
-});
-
-dialogPopup.addEventListener("click", (evt) => {
-  if (evt.target.className === "popup_dialog") {
-    evt.preventDefault();
-    dialogPopup.close();
-  }
-});
-
-formsPopupNewPlaces.addEventListener("click", (evt) => {
-  if (evt.target.className === "form__dialog") {
-    evt.preventDefault();
-    formsPopupNewPlaces.close();
-    handleClosePopup(evt);
-  }
-});
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,12 @@
 const dialogPopup = document.querySelector(".popup_dialog");
+const formsPopup = document.querySelector(".popup");
+const overlayContainer = document.querySelector(".popup__overlay");
+const openProfilePopup = document.querySelector(".profile__edit-button");
+const closePlacePopup = document.querySelector("#place-close-button");
+const formsPopupNewPlaces = document.querySelector("#popup__new-places");
+const closeProfilePopup = document.querySelector(".forms__close-button");
+const closeFormProfil = document.querySelector(".forms-profile-button");
+const openNewPlacePopup = document.querySelector(".profile__add-button");
 
 export const initialCards = [
   {
@@ -35,3 +43,54 @@ export function openImage(item) {
   dialogImge.alt = item.name;
   dialogDescription.textContent = item.name;
 }
+export function handleOpenPopup() {
+  formsPopup.classList.add("popup__show");
+  overlayContainer.classList.add("overlay_show");
+}
+export function handleClosePopup(evt) {
+  evt.preventDefault();
+  formsPopup.classList.remove("popup__show");
+  overlayContainer.classList.remove("overlay_show");
+  formsPopupNewPlaces.close();
+  dialogPopup.close();
+  validateFormProfile._resetForm();
+  validateFormNewBike._resetForm();
+}
+
+openProfilePopup.addEventListener("click", handleOpenPopup);
+
+closeProfilePopup.addEventListener("click", handleClosePopup);
+
+closeFormProfil.addEventListener("click", handleClosePopup);
+
+overlayContainer.addEventListener("click", handleClosePopup);
+
+openNewPlacePopup.addEventListener("click", () => {
+  formsPopupNewPlaces.showModal();
+});
+
+closePlacePopup.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  formsPopupNewPlaces.close();
+  handleClosePopup(evt);
+});
+dialogPopup.addEventListener("click", (evt) => {
+  if (evt.target.className === "popup_dialog") {
+    evt.preventDefault();
+    dialogPopup.close();
+  }
+});
+
+formsPopupNewPlaces.addEventListener("click", (evt) => {
+  if (evt.target.className === "form__dialog") {
+    evt.preventDefault();
+    formsPopupNewPlaces.close();
+    handleClosePopup(evt);
+  }
+});
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    handleClosePopup(evt);
+  }
+});
+import { validateFormNewBike, validateFormProfile } from "./index.js";
